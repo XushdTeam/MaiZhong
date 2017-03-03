adb24f5cfbf6905713069600573ef1e87e983779
Xushd <xsd529@163.com>
2017-03-03 17:58:56 +0800

    商城框架搭建完成

---
+++ Portal/src/main/java/com/maizhong/portal/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.maizhong.portal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 商城首页
+ * Created by Xushd on 2017/3/3.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "index";
+    }
+}
---
+++ Portal/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Xushd
+  Date: 2017/3/3
+  Time: 17:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta name="renderer" content="webkit" />
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
