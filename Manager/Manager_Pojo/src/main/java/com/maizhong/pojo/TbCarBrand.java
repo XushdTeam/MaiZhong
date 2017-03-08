965e03b930e37816c67840d4b100f0c0df6c59ca
wangforjava <WANGhao156>
2017-03-08 09:25:10 +0800

    后台类型管理，包含品牌管理和类别管理

---
+++ Manager/Manager_Pojo/src/main/java/com/maizhong/pojo/TbCarBrand.java
@@ -0,0 +1,63 @@
+package com.maizhong.pojo;
+
+public class TbCarBrand {
+    private Long id;
+
+    private String brandName;
+
+    private String brandImg;
+
+    private Integer brandSequence;
+
+    private Integer status;
+
+    private Integer delflag;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName == null ? null : brandName.trim();
+    }
+
+    public String getBrandImg() {
+        return brandImg;
+    }
+
+    public void setBrandImg(String brandImg) {
+        this.brandImg = brandImg == null ? null : brandImg.trim();
+    }
+
+    public Integer getBrandSequence() {
+        return brandSequence;
+    }
+
+    public void setBrandSequence(Integer brandSequence) {
+        this.brandSequence = brandSequence;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getDelflag() {
+        return delflag;
+    }
+
+    public void setDelflag(Integer delflag) {
+        this.delflag = delflag;
+    }
+}
