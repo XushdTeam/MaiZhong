64b00cf665c127e92f7db8a687513a9e4e746abf
Xushd <xsd529@163.com>
2017-03-30 11:26:59 +0800

    加入图片上传 方法

--- BRest/src/main/java/com/maizhong/bRest/service/impl/ImgUploadServiceImpl.java
+++ BRest/src/main/java/com/maizhong/bRest/service/impl/ImgUploadServiceImpl.java
@@ -3,11 +3,13 @@ package com.maizhong.bRest.service.impl;
 import com.maizhong.bRest.service.ImgUploadService;
 import com.maizhong.common.enums.OperateEnum;
 import com.maizhong.common.result.JsonResult;
-import com.maizhong.common.utils.FtpUtil;
-import com.maizhong.common.utils.IDUtils;
-import com.maizhong.common.utils.TimeUtils;
+import com.maizhong.common.utils.FileUploadUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Xushd on 2017/3/29.
@@ -15,34 +17,59 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class ImgUploadServiceImpl implements ImgUploadService {
 
+    // aliOSS
+
+    @Value("${ENDPOINT}")
+    private String ENDPOINT;
+
+    @Value("${ACCESSKEYID}")
+    private String ACCESSKEYID;
+
+    @Value("${ACCESSKEYSECRET}")
+    private String ACCESSKEYSECRET;
+
+    @Value("${BUCKETNAME}")
+    private String BUCKETNAME;
+
+    @Value("${IMGURL}")
+    private String IMGURL;
+
+
     @Override
-    public String uploadImg(MultipartFile filedata) {
-//        try{
-//            if (filedata.isEmpty()) return JsonResult.build(OperateEnum.FILE_EMPTY);
-//            if (filedata.getSize() > 500 * 1024) return JsonResult.build(OperateEnum.FILE_SIZE);
-//
-//            String filePath = "/"+ TimeUtils.getYear()+"/"+TimeUtils.getMonth()+"/"+TimeUtils.getDay();
-//
-//            String originalFilename = filedata.getOriginalFilename();
-//            //新文件名
-//            String newFileName = IDUtils.genImageName()+originalFilename.substring(originalFilename.lastIndexOf("."));
-//            //转存文件,上传到FTP
-//            FtpUtil.uploadFile(FTP_SERVER_IP,FTP_SERVER_PORT,FTP_SERVER_USERNAME,FTP_SERVER_PASSWORD
-//                    ,pathKey,filePath,newFileName,filedata.getInputStream());
-//
-//            return JsonResult.build(200,OperateEnum.FILE_UPLOAD_SUCCESS.getStateInfo(),IMAGE_BASE_URL+pathKey+filePath+"/"+newFileName);
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            return JsonResult.build(OperateEnum.FAILE);
-//        }
-//    }
-//
+    public JsonResult uploadImg(MultipartFile filedata, String key) {
 
+        try {
+            if (filedata.isEmpty()) return JsonResult.build(OperateEnum.FILE_EMPTY);
+            if (filedata.getSize() > 500 * 1024) return JsonResult.build(OperateEnum.FILE_SIZE);
 
+            Map param = getParam();
+            String originalFilename = filedata.getOriginalFilename();
+            param.put("originalFilename", originalFilename);
+            param.put("key", key);
 
+            String newFileName = FileUploadUtil.UploadFileOSS(param, filedata.getBytes());
 
+            return JsonResult.build(200, OperateEnum.FILE_UPLOAD_SUCCESS.getStateInfo(), IMGURL + key + newFileName);
 
-        return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return JsonResult.build(OperateEnum.FAILE);
+        }
+    }
+
+    /**
+     * 获取OSS对象参数
+     *
+     * @return
+     */
+    private Map<String, String> getParam() {
+
+        Map<String, String> param = new HashMap<>();
+        param.put("endpoint", ENDPOINT);
+        param.put("accessKeyId", ACCESSKEYID);
+        param.put("accessKeySecret", ACCESSKEYSECRET);
+        param.put("bucketName", BUCKETNAME);
+
+        return param;
     }
 }
