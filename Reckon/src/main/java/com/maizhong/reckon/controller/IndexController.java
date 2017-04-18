abaade016a493265bfb98fff89a2da13fe4884de
Xushd <xsd529@163.com>
2017-04-18 16:04:48 +0800

    追加测试跳转

--- Reckon/src/main/java/com/maizhong/reckon/controller/IndexController.java
+++ Reckon/src/main/java/com/maizhong/reckon/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.maizhong.reckon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,5 +14,9 @@ public class IndexController {
     @RequestMapping(value = "/")
     public String index(){
         return "index";
+    }
+    @RequestMapping(value = "/{page}")
+    public String test(@PathVariable String page){
+        return page;
     }
 }
