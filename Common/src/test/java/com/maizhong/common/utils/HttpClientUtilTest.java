139eb8ecb48ab40255426d699f068d1df0dd4333
Xushd <xsd529@163.com>
2017-04-20 20:16:26 +0800

    预约

---
+++ Common/src/test/java/com/maizhong/common/utils/HttpClientUtilTest.java
@@ -0,0 +1,25 @@
+package com.maizhong.common.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Xushd on 2017/3/13.
+ */
+public class HttpClientUtilTest {
+
+    @Test
+    public void doGet() throws Exception {
+
+//        String s = HttpClientUtil.doGet("http://www.baidu.com");
+    //    System.out.println(s);
+
+    }
+
+    @Test
+    public void doPost() throws Exception {
+
+    }
+
+}
